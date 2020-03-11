b4f56442d0f99cab0383e399fadbfcb35c1a9ca5
Manuel Corona <coronamvirgo@gmail.com>
2020-03-11 13:20:01 -0400

    heroku deployment

--- models/Article.js
+++ models/Article.js
@@ -1,21 +1,33 @@
-const mongoose = require('mongoose');
+var mongoose = require("mongoose");
 
+// Save a reference to the Schema constructor
+//===========================================
 var Schema = mongoose.Schema;
-var ArticleSchema = new Schema ({
+
+// Using the Schema constructor, create a new UserSchema object
+// =============================================================
+var ArticleSchema = new Schema({
     title: {
         type: String,
         required: true
-    }, 
+    },
     link: {
-        type: String, 
+        type: String,
         required: true
     },
-    comment: [{
-        type: Schema.Types.ObjectId, 
-        ref: "Comment"
+    excerpt: {
+        type: String
+    },
+    note: [{
+        type: Schema.Types.ObjectId,
+        ref: "Note"
     }]
-})
+});
 
+// This creates our model from the above schema, using mongoose's model method
+//=============================================================================
 var Article = mongoose.model("Article", ArticleSchema);
 
+// Export the Article model
+//============================
 module.exports = Article;
